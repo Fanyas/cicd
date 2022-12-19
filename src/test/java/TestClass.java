277659ff1fa4ee4badecd3648f9c398f157b3c49
Stipizzz <gaidukov0423@gmail.com>
2022-12-19 19:48:40 +0300

    add test class

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,20 @@
+import org.example.Calc;
+
+import static org.example.Calc.plus;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.*;
+
+public class TestClass {
+    @org.junit.jupiter.api.Test
+    void trueAssumption() {
+        assumeTrue(plus(2, 2) == 5);
+        assertEquals(plus(2,2),4);
+    }
+
+    @org.junit.jupiter.api.Test
+    void falseAssumption() {
+        assumeFalse(false);
+        assumeFalse(plus(2, 2) == 5);
+        assertEquals(5 + 2, 7);
+    }
+}
