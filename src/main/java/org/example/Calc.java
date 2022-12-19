e0daf49935e4cb4216b46f2c0966986525a67f33
Stipizzz <gaidukov0423@gmail.com>
2022-12-19 19:35:18 +0300

    init

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Calc {
+    public static int plus(int a, int b){
+        return a + b;
+    }
+    public static int minus(int a, int b){
+        return a - b;
+    }
+    public static double multi(int a, int b){
+        return a * b;
+    }
+    public static double div(int a, int b){
+        return a / b;
+    }
+}
